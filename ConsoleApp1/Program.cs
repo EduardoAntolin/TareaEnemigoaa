5e6fa8a382cb6b960d7a4601ed96c6efe360dc06
EduardoAntolin <42181304+EduardoAntolin@users.noreply.github.com>
2018-08-15 21:50:52 -0700

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TareaVideoJuego
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CaracteristicasDelEnemigo enemigo = new CaracteristicasDelEnemigo();
+
+            enemigo.PosicionEnXDelEnemigo = "La posicion del enemigio en X es: ";
+            enemigo.PosicionEnYDelEnemigo = "La posicion del enemigo en Y es: ";
+            enemigo.Moverse = " Se ha movido a: ";
+            enemigo.Disparar = " El Enemigo ha disparado. ";
+            enemigo.Explotar = " El enemigo exploto, cuidado! ";
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp1/enemigo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TareaVideoJuego
+{
+    class CaracteristicasDelEnemigo
+    {
+        //Propiedades * caracteristicas enemigo
+        public string PosicionEnYDelEnemigo { get; set; }
+        public string PosicionEnXDelEnemigo { get; set; }
+        public string Explotar { get; set; }
+        public string Moverse { get; set; }
+        public string Disparar { get; set; }
+
+
+        private string _puntos;
+        public string Puntos
+        {
+            get
+            {
+                return _puntos;
+            }
+            set
+            {
+                if (value.Length < 100)
+                {
+                    _puntos = "El enemigo deberia dar mas de 100 puntos";
+                }
+                else
+                {
+                    _puntos = "El enemigo te dio mas de 100 puntos";
+                }
+            }
+        }
+
+    }
+}
